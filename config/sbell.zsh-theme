7276b09eb8e2ca79f63d439d706b0632aab0c856
Wenzel Jakob <wenzel@cs.cornell.edu>
2012-10-19 21:20:13 -0400

    nicer tools for displaying the version control status

--- config/sbell.zsh-theme
+++ config/sbell.zsh-theme
@@ -1,11 +1,5 @@
-# https://github.com/blinks zsh theme
-
-function _prompt_char() {
-  if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
-    echo "%{%F{green}%}±%{%f%k%b%}"
-  else
-    echo ' '
-  fi
+function vcs_prompt_info() {
+	vcprompt -f ' [%%F{blue}%b%%F{red}%s%u%m%%B%%F{green}]'
 }
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{blue}%}"
@@ -13,8 +7,9 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%K{black}%B%F{green}%}]"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT='%{%f%k%b%}
-%{%K{black}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{black}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
-%{%K{black}%}$(_prompt_char)%{%K{black}%} %#%{%f%k%b%} '
+ZSH_THEME_HG_PROMPT_PREFIX=$ZSH_THEME_GIT_PROMPT_PREFIX
+ZSH_THEME_HG_PROMPT_SUFFIX=$ZSH_THEME_GIT_PROMPT_SUFFIX
 
-#RPROMPT='!%{%B%F{cyan}%}%!%{%f%k%b%}'
+PROMPT='%{%f%k%b%}
+%{%K{black}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{black}%}%~%{%B%F{green}%}$(vcs_prompt_info)%E%{%f%k%b%}
+%{%K{black}%}%{%K{black}%} %#%{%f%k%b%} '
---
+++ vcprompt/tests/common.sh
@@ -0,0 +1,88 @@
+# where to find all the test files
+testdir=`pwd`
+
+# other globals set by functions here
+vcprompt=""
+tmpdir=""
+
+LANG=
+PATH=$testdir/..:$PATH
+
+die()
+{
+    msg=$1
+    echo "error: $msg" >&2
+    exit 1
+}
+
+# Check if some external command is available by running it
+# and ensuring that it prints an expected string.  If not,
+# exit with optional message.
+check_available()
+{
+    cmd=$1
+    expect=$2
+    msg=$3
+
+    if ! $cmd 2>/dev/null | grep -q "$expect"; then
+        [ "$msg" ] && echo $msg
+        exit 0
+    fi
+}
+
+find_vcprompt()
+{
+    vcprompt=$testdir/../vcprompt
+    [ -x $vcprompt ] ||
+        die "vcprompt executable not found (expected $vcprompt)"
+}
+
+setup()
+{
+    tmpdir=`mktemp -d /tmp/vcprompt.XXXXXX`
+    [ -n "$tmpdir" -a -d "$tmpdir" ] ||
+        die "unable to create temp dir '$tmpdir'"
+    trap cleanup 0 1 2 15
+}
+
+cleanup()
+{
+    echo "cleaning up $tmpdir"
+    chmod -R u+rwx $tmpdir
+    rm -rf $tmpdir
+}
+
+assert_vcprompt()
+{
+    message=$1
+    expect=$2
+    format=$3
+    if [ -z "$format" ]; then
+        format="%b"
+    fi
+
+    if [ "$format" != '-' ]; then
+        actual=`VCPROMPT_FORMAT="$VCPROMPT_FORMAT" $vcprompt -f "$format"`
+    else
+        actual=`VCPROMPT_FORMAT="$VCPROMPT_FORMAT" $vcprompt`
+    fi
+
+    if [ "$expect" != "$actual" ]; then
+        echo "fail: $message: expected \"$expect\", but got \"$actual\"" >&2
+        failed="y"
+        return 1
+    else
+        echo "pass: $message"
+    fi
+}
+
+report()
+{
+    if [ "$failed" ]; then
+	echo "$0: some tests failed"
+	exit 1
+    else
+	echo "$0: all tests passed"
+	exit 0
+    fi
+}
